31f74860834cb5e958a1a5c99c8bd16fab9030dc
Lucas Kalks <lucaskalks00@gmail.com>
2022-11-10 20:01:28 -0300

    Configurando PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,12 @@ import { environment } from 'src/environments/environment'
       or after 30 seconds (whichever comes first)
       */
       registrationStrategy: 'registerWhenStable:30000'
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   providers: [],
