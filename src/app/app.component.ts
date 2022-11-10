784c825a98efc9886bdc395fefa32ac849f327fd
Lucas Kalks <lucaskalks00@gmail.com>
2022-11-10 19:36:27 -0300

    Aplicativo configurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appGeo';
+
+  map_url = ''
+  latitude = 0
+  longitude = 0
+
+  latitude2 = 0
+  longitude2 = 0
+
+  ngOnInit(){
+    this.verifica_local()
+  }
+
+  verifica_local(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.latitude = pos.coords.latitude
+          this.longitude = pos.coords.longitude
+          this.map_url = 'https://www.google.com/maps/search/?api=1&query='+this.latitude+','+this.longitude
+        },
+        () => {
+          alert('Sem localização!')
+        },
+        { timeout: 15000 }
+      )
+    }
+
+    
+  }
 }
